040a1eb6c6670a0e38cd0a4b5c316a1ca75f00e5
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-04-03 23:19:27 +0200

    updates

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,32 +1,22 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data')
 
-app.get('/', (req, res)=>{
-    res.send('<h1> Home Page </h1><a href="/api/products"></a>')
+// req => middleware => res
+
+const logger = (req,res,next) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    next()
+}
+
+app.get('/', logger,(req,res) => {  
+    res.send('Home')
 })
 
-app.get('/api/products',(req, res)=>{
-    const newProduct = products.map((product)=> {
-        const {id, name, image} = product;
-        return {id, name ,image}
-    })
-    res.json(newProduct)
-})
-
-app.get('/api/products/:productID',(req, res)=>{
-    // console.log(req)
-    // console.log(req.params)
-    const {productID} = req.params;
-
-    const singleProduct = products.find((product) => product.id === Number(productID))
-
-    if(!singleProduct){
-        return res.status(404).send('Product Does Not Exist')
-    }
-
-    console.log(singleProduct);
-    res.json(singleProduct)
+app.get('/about', (req,res) => {
+    res.send('About')
 })
 
 app.listen(5000, ()=>{
